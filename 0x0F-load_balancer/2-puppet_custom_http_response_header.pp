014572d93d6aa8e6acf12581b0ed1d8c28c0cb96
j4ir0st <jairocast2@gmail.com>
2022-04-25 21:20:40 -0500

    puppet for HAProxy config removing backslash

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,5 +7,5 @@ exec { 'balancer':
 	      sudo apt-get install haproxy -y;\
 	      sudo sed -i "$ a \\\nfrontend haproxy-main\n\tbind *:80\n\tdefault_backend nginx_servers" /etc/haproxy/haproxy.cfg;\
 	      sudo sed -i "$ a \\\nbackend nginx_servers\n\tbalance roundrobin\n\tserver web-01 35.227.55.132:80\n\tserver web-02 34.207.110.152:80" /etc/haproxy/haproxy.cfg;\
-	      sudo service haproxy restart;\',
-     }
+	      sudo service haproxy restart',
+}
