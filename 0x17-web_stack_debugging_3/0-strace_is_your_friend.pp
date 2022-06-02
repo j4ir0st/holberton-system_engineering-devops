ef97d2f9c355c7b971105d94ffdde8085be8571a
j4ir0st <jairocast2@gmail.com>
2022-06-02 10:32:52 -0500

    Web stack debugging 3 - fixing with strace fixing puppet error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Web stack debugging 3 - fixing with strace
 exec {'web_debugging3':
   provider => shell,
-  command  => 'sudo sed -i "s/.phpp /.php /" /var/www/html/wp-settings.php;\
-  	       sudo service apache2 restart',
+  command  => 'sudo sed -i "s/require_once( ABSPATH . WPINC . \'\/class-wp-locale\.phpp\' );/require_once( ABSPATH . WPINC . \'\/class-wp-locale.php\' );/" /var/www/html/wp-settings.php;\
+               sudo service apache2 restart',
 }
