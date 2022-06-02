b172c0f91c0b90ce1975a73129df5bda2e91bade
j4ir0st <jairocast2@gmail.com>
2022-06-02 12:56:28 -0500

    returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function to get number of suscribers """
+    url = "https://www.reddit.com/r/"
+    headers = {"User-Agent": "j4ir0st"}
+    sub = "{}{}/about.json".format(url, subreddit)
+    req = requests.get(sub, headers=headers, allow_redirects=False).json()
+    subs = req.get("data").get("subscribers")
+    return subs
